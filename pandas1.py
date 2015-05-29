20c992d03a2edf36499061209e98002216375630
Shantnu Tiwari <a@a.com>
2015-05-29 16:24:14 +0100

    Replacing morbid parts

--- pandas1.py
+++ pandas1.py
@@ -11,34 +11,41 @@ print("Total rows: {0}".format(len(data)))
 print("\n Accidents")
 print("-----------")
 
-# Accidents which killed > 10 people
-ten_deaths = data[data.Number_of_Casualties > 10]
-print("> 10 people died: {0}".format(
+# Accidents which happened on a Sunday
+ten_deaths = data[data.Day_of_Week == 1]
+print("Accidents which happened on a Sunday: {0}".format(
     len(ten_deaths)))
 
-# Accidents which killed > 10 people, > 20 cars
+# Accidents which happened on a Sunday, > 20 cars
 ten_deaths_twenty_cars = data[
-    (data.Number_of_Casualties > 10) & (data.Number_of_Vehicles > 20)]
-print("> 10 people died involving > 20 cars: {0}".format(
+    (data.Day_of_Week == 1) & (data.Number_of_Vehicles > 20)]
+print("Accidents which happened on a Sunday involving > 20 cars: {0}".format(
     len(ten_deaths_twenty_cars)))
 
-# Accidents which killed > 10 people, > 20 cars, in the rain
+# Accidents which happened on a Sunday, > 20 cars, in the rain
 ten_deaths_twenty_cars_rain = data[
-    (data.Number_of_Casualties > 10) & (data.Number_of_Vehicles > 20) &
+    (data.Day_of_Week == 1) & (data.Number_of_Vehicles > 20) &
     (data.Weather_Conditions == 2)]
-print("> 10 people died involving > 20 cars in the rain: {0}".format(
+print("Accidents which happened on a Sunday involving > 20 cars in the rain: {0}".format(
     len(ten_deaths_twenty_cars_rain)))
 
 # All the accidents in London from 1979-2004
-london_accidents = data[data['Police_Force'] == 1]
-london_accidents_date_casualties = london_accidents[[
-    'Date', 'Number_of_Casualties']]
-print("\nTotal accidents in London from 1979-2004: {0}\n".format(
-    len(london_accidents_date_casualties)))
-print(london_accidents_date_casualties.head(10))
+london_data = data[data['Police_Force'] == 1]
+
+# Convert date to Pandas date/time
+london_data['Date'] = pd.to_datetime(london_data['Date'])
+#print(london_data['Date'][:5])
+
+
+# Extract the data for only August 2000
+london_data_aug_2000 = london_data[
+    (london_data['Date'] > pd.to_datetime('2000-08-01')) &
+    (london_data['Date'] < pd.to_datetime('2000-08-30'))
+]
+
 
 # Save to Excel
 writer = pd.ExcelWriter(
-    'Accidents_London.xlsx', engine='xlsxwriter')
-london_accidents_date_casualties.to_excel(writer, 'Sheet1')
+    'London_Aug_2000.xlsx', engine='xlsxwriter')
+london_data_cars.to_excel(writer, 'Sheet1')
 writer.save()
